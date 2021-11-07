e60a8536245399e479c10541c0511e43a5a5ae79
Pavel Konarikh <paveldr9514@gmail.com>
2021-11-07 16:14:09 +0700

    Contacts

--- script/app.js
+++ script/app.js
@@ -33,7 +33,9 @@ navLink.forEach(n => n.addEventListener('click', linkAction));
 function scrollHeader() {
     const header = document.getElementById('header');
     // When the scroll is greater than 80 viewport height, add the scroll-header class to the header tag
-    if (this.scrollY >= 80) { header.classList.add('scroll-header'); } else { header.classList.remove('scroll-header'); }
+    if (this.scrollY >= 80) {
+        header.classList.add('scroll-header');
+    } else { header.classList.remove('scroll-header'); }
 }
 window.addEventListener('scroll', scrollHeader);
 
@@ -59,7 +61,7 @@ const toggleItem = (item) => {
         accordionContent.removeAttribute('style');
         item.classList.remove('accordion-open');
     } else {
-        accordionContent.style.height = accordionContent.scrollHeight + 'px'
+        accordionContent.style.height = accordionContent.scrollHeight + 'px';
         item.classList.add('accordion-open');
     }
 }
